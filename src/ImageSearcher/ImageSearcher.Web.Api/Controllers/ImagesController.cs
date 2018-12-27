7f166e6ec8ec8cdfaf4167d6a2794a855a66ccac
d.trofimov <d.trofimov@cloudpayments.ru>
2018-12-27 23:30:31 +0300

    Add web api with swagger

---
+++ src/ImageSearcher/ImageSearcher.Web.Api/Controllers/ImagesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using ImageSearcher.Core;
+using ImageSearcher.Core.DTO;
+using ImageSearcher.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSearcher.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImagesController : Controller
+    {
+        private readonly IImageService _imageService;
+
+        public ImagesController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return BadRequest();
+            }
+
+            var result = await _imageService.GetByIdAsync(id);
+
+            return GetActionResult(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get([FromQuery]ImageFilter filter)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _imageService.SearchAsync(filter);
+
+            return GetActionResult(result);
+        }
+
+        private ActionResult GetActionResult<TModel>(QueryResult<TModel> result)
+            where TModel : class
+        {
+            return result.Code == HttpStatusCode.OK
+                ? Ok(result.Model)
+                : StatusCode((int)result.Code, result.Message);
+        }
+    }
+}
---
+++ src/ImageSearcher/ImageSearcher.Web.Api/Startup.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using ImageSearcher.Core.Interfaces;
+using ImageSearcher.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ImageSearcher.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<HttpClient>();
+            services.AddSingleton<IHttpHandler, HttpHandler>();
+            services.AddSingleton<IImageService, ImageService>(x => new ImageService(x.GetService<IHttpHandler>(), Configuration["FlickrApiKey"]));
+            services.AddAutoMapper();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+        }
+    }
+}
