e529bce3f6d7d0d86a2440342ca72fa9fed91e71
d.trofimov <d.trofimov@cloudpayments.ru>
2018-12-28 18:30:56 +0300

    Fix api tests

--- src/ImageSearcher/ImageSearcher.Web.Api.Tests/TestStartup.cs
+++ src/ImageSearcher/ImageSearcher.Web.Api.Tests/TestStartup.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Net.Http;
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using ImageSearcher.Core.Interfaces;
+using ImageSearcher.Infrastructure.MapperProfiles;
+using ImageSearcher.Web.Api.Models.MapperProfiles;
+using ImageSearcher.Web.Api.Models.Request;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ImageSearcher.Web.Api.Tests
 {
@@ -6,6 +15,25 @@ namespace ImageSearcher.Web.Api.Tests
     {
         public TestStartup(IConfiguration configuration) : base(configuration)
         {
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddFluentValidation();
+            services.AddTransient<IValidator<SearchImages>, SearchImagesValidator>();
+            services.AddSingleton<HttpClient>();
+            services.AddSingleton<IHttpHandler, TestHttpHandler>();
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<SearchProfile>();
+                cfg.AddProfile<GetByIdProfile>();
+                cfg.AddProfile<ImageFilterProfile>();
+            });
+            var mapper = configuration.CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddSingleton<IImageService, TestImageService>();
+            services.AddScoped<ICachingService, TestCachingService>();
         }
     }
 }
