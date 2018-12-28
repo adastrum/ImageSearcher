d1c05382678bb572bb7bdeef9e9157244c76387e
d.trofimov <d.trofimov@cloudpayments.ru>
2018-12-28 15:44:19 +0300

    Add caching service for redis

---
+++ src/ImageSearcher/ImageSearcher.Core/Interfaces/ICachingService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ImageSearcher.Core.Interfaces
+{
+    public interface ICachingService
+    {
+        Task SetAsync<TValue>(string key, TValue value)
+            where TValue : class;
+
+        Task<TValue> GetAsync<TValue>(string key)
+            where TValue: class;
+    }
+}
---
+++ src/ImageSearcher/ImageSearcher.Infrastructure/CachingService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using ImageSearcher.Core.Interfaces;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace ImageSearcher.Infrastructure
+{
+    public class CachingService : ICachingService
+    {
+        private readonly IDatabase _database;
+
+        public CachingService(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public async Task SetAsync<TValue>(string key, TValue value)
+            where TValue : class
+        {
+            var serialized = JsonConvert.SerializeObject(value);
+            await _database.StringSetAsync(key, serialized);
+        }
+
+        public async Task<TValue> GetAsync<TValue>(string key)
+            where TValue : class
+        {
+            try
+            {
+                var serialized = await _database.StringGetAsync(key);
+                return JsonConvert.DeserializeObject<TValue>(serialized);
+            }
+            catch (Exception exception)
+            {
+                //todo: log
+                return await Task.FromResult<TValue>(null);
+            }
+        }
+    }
+}
